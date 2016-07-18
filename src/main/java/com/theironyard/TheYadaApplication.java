3ac5c87ec071fdb66f874ce32d016accab29c83c
jonathandavidblack <jonnybl915@gmail.com>
2016-07-18 13:50:47 -0400

    initial import

---
+++ src/main/java/com/theironyard/TheYadaApplication.java
@@ -0,0 +1,12 @@
+package com.theironyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheYadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheYadaApplication.class, args);
+	}
+}
