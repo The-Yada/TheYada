4e59fc76a72de3a489f219020279afdd45000098
willriggins <williamriggins2013@gmail.com>
2016-07-18 16:51:49 -0400

    starting pagination

--- src/main/java/com/theironyard/controllers/YadaRestController.java
+++ src/main/java/com/theironyard/controllers/YadaRestController.java
@@ -1,7 +1,47 @@
 package com.theironyard.controllers;
 
+import com.theironyard.entities.Yada;
+import com.theironyard.services.UserRepository;
+import com.theironyard.services.YadaRepository;
+import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.sql.SQLException;
+
 /**
  * Created by will on 7/18/16.
  */
+@RestController
 public class YadaRestController {
+
+    @Autowired
+    UserRepository users;
+
+    @Autowired
+    YadaRepository yadas;
+
+    // start h2 database
+    @PostConstruct
+    public void init() throws SQLException {
+        Server.createWebServer().start();
+    }
+
+    // get route for yadas
+    @RequestMapping(path = "/yadas", method = RequestMethod.GET)
+    public Page<Yada> getYadas(Integer page, double weight) {
+
+        page = (page == null) ? 0 : page;
+        PageRequest pageRequest = new PageRequest(page, 10);
+
+        Page<Yada> pageOfYadas;
+        pageOfYadas = yadas.findByWeightDesc(pageRequest, weight);
+
+        return pageOfYadas;
+    }
 }
