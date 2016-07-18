4065a636b1265b543926a8dbaec5dcd6ef2085ed
jonathandavidblack <jonnybl915@gmail.com>
2016-07-18 15:28:38 -0400

    added YadaUserJoinRepository and began yada parsing in the YadaController

--- src/main/java/com/theironyard/controllers/YadaController.java
+++ src/main/java/com/theironyard/controllers/YadaController.java
@@ -1,7 +1,52 @@
 package com.theironyard.controllers;
 
+import com.theironyard.entities.Yada;
+import com.theironyard.services.UserRepository;
+import com.theironyard.services.YadaRepository;
+import com.theironyard.services.YadaUserJoinRepository;
+import org.h2.tools.Server;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
 /**
  * Created by will on 7/18/16.
  */
+@Controller
 public class YadaController {
+
+    @Autowired
+    UserRepository users;
+
+    @Autowired
+    YadaRepository yadas;
+
+    @Autowired
+    YadaUserJoinRepository yadaUserJoinRepo;
+
+    @PostConstruct
+    public void init() throws SQLException, FileNotFoundException {
+        Server.createWebServer().start();
+        if (yadas.count() == 0) {
+            parseYadas("yadas.csv");
+        }
+    }
+    public void parseYadas(String fileName) throws FileNotFoundException {
+        if (yadas.count() == 0) {
+            File yadaFile = new File(fileName);
+            Scanner fileScanner = new Scanner(yadaFile);
+            fileScanner.nextLine();
+            while (fileScanner.hasNext()) {
+                String[] columns = fileScanner.nextLine().split(",");
+                Yada yada = new Yada();
+                yadas.save(yada);
+
+            }
+
 }
---
+++ src/main/java/com/theironyard/services/YadaUserJoinRepository.java
@@ -0,0 +1,10 @@
+package com.theironyard.services;
+
+import com.theironyard.entities.YadaUserJoin;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jonathandavidblack on 7/18/16.
+ */
+public interface YadaUserJoinRepository extends CrudRepository<YadaUserJoin, Integer> {
+}
