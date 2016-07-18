b9a31b10e37e65a88c48c50525cfd3b4f4657faa
jonathandavidblack <jonnybl915@gmail.com>
2016-07-18 14:43:15 -0400

    added User and YadaUserJoin classes

---
+++ src/main/java/com/theironyard/entities/YadaUserJoin.java
@@ -0,0 +1,58 @@
+package com.theironyard.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+/**
+ * Created by jonathandavidblack on 7/18/16.
+ */
+@Table(name = "yadaUserJoins")
+public class YadaUserJoin {
+
+    @Id
+    @GeneratedValue
+    int id;
+
+    @JsonIgnore
+    @ManyToOne
+    User user;
+
+    @ManyToOne
+    Yada yada;
+
+    public YadaUserJoin(int id, User user, Yada yada) {
+        this.id = id;
+        this.user = user;
+        this.yada = yada;
+    }
+
+    public YadaUserJoin(User user, Yada yada) {
+        this.user = user;
+        this.yada = yada;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Yada getYada() {
+        return yada;
+    }
+
+    public void setYada(Yada yada) {
+        this.yada = yada;
+    }
+}
