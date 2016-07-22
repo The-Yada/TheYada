010476411267e182209cd9b96b82eefadafb40fb
jonathandavidblack <jonnybl915@gmail.com>
2016-07-22 16:01:19 -0400

    Merge branch 'master' of https://github.com/The-Yada/TheYada
    # Conflicts:
    #	src/main/java/com/theironyard/controllers/YadaRestController.java

--- src/main/java/com/theironyard/controllers/YadaRestController.java
+++ src/main/java/com/theironyard/controllers/YadaRestController.java
@@ -14,6 +14,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
