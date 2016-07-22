a2854808c9b8e61599d099f418235ffd9d161fcb
Cbodtorf <cbodtorf@gmail.com>
2016-07-21 21:56:43 -0400

    new oauth w/ satellizer

--- js/controllers/nav-controller.js
+++ js/controllers/nav-controller.js
@@ -14,13 +14,7 @@ module.exports = function(app) {
     // $scope.user = UserService.getUser();
     $scope.isCollapsed = false;
 
-    function signOut() {
-      GoogleAuth.signOut()
-      var auth2 = gapi.auth2.getAuthInstance();
-      auth2.signOut().then(function () {
-        console.log('User signed out.');
-      });
-    }
+
 
 
 
