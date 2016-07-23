f8ca12233ff9fd273d5970051ee751a392a00456
jonathandavidblack <jonnybl915@gmail.com>
2016-07-23 14:46:21 -0400

    refactored get routes and now have successful communication to the chrome extension

--- src/main/java/com/theironyard/controllers/YadaRestController.java
+++ src/main/java/com/theironyard/controllers/YadaRestController.java
@@ -95,7 +95,7 @@ public class YadaRestController {
     }
 
     //route which brings user to the editing screen with scraped website text and submission box
-    @RequestMapping(path = "/lemmieYada/{url}", method = RequestMethod.GET)
+    @RequestMapping(path = "/lemmieYada{url}", method = RequestMethod.GET)
     public ArrayList<String> letMeYada(@PathVariable String url) throws IOException {
 
         ArrayList<String> scrapedSite = soupThatSite(url);
@@ -131,7 +131,6 @@ public class YadaRestController {
         yadas.save(yada);
 
     }
-
     //hit this route so users can upVote yadas
     @RequestMapping(path = "/upVote", method = RequestMethod.POST)
     public HttpStatus upVote(int yadaUserJoinId, int userId, int yadaId){
@@ -153,7 +152,6 @@ public class YadaRestController {
 
         return HttpStatus.ACCEPTED;
     }
-
     //hit this route so users can downVote yadas
     @RequestMapping(path = "/downVote", method = RequestMethod.POST)
     public HttpStatus downVote(int yadaUserJoinId, int userId, int yadaId){
@@ -177,12 +175,13 @@ public class YadaRestController {
     }
 
     //hit this route to display yadas for a given webpage from the chrome extension
-    @RequestMapping(path = "/lemmieSeeTheYadas/{url}", method = RequestMethod.GET)
-    public ArrayList<Yada> showMeTheYada(@PathVariable String url) {
+    @RequestMapping(path = "/lemmieSeeTheYadas{id}", method = RequestMethod.GET)
+    public ArrayList<Yada> showMeTheYada(String url) {
 
         Link link = links.findFirstByUrl(url);
+        ArrayList<Yada> theYadas = (ArrayList<Yada>) link.getYadaList();
 
-        return (ArrayList<Yada>) link.getYadaList();
+        return theYadas;
     }
 
     @RequestMapping(path = "/addYada", method = RequestMethod.POST)
