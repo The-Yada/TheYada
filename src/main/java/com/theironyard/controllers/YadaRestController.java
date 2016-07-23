9d60d80db1f911eb119e9b16e9960e206709002d
jonathandavidblack <jonnybl915@gmail.com>
2016-07-23 16:59:32 -0400

    added spacing and login check to gimmieTheYadas route

--- src/main/java/com/theironyard/controllers/YadaRestController.java
+++ src/main/java/com/theironyard/controllers/YadaRestController.java
@@ -90,14 +90,6 @@ public class YadaRestController {
         return links.findTop5ByOrderByLinkScoreDesc();
     }
 
-    //route which brings user to the editing screen with scraped website text and submission box
-    @RequestMapping(path = "/lemmieYada", method = RequestMethod.GET)
-    public ArrayList<String> letMeYada(@RequestParam (value = "url", required = false) String url) throws IOException {
-
-        ArrayList<String> scrapedSite = soupThatSite(url);
-
-        return scrapedSite;
-    }
     // algo attempt 1
     public List<Link> generateLinkScore(ArrayList<Link> linkList) {
 
@@ -127,6 +119,8 @@ public class YadaRestController {
         yadas.save(yada);
 
     }
+
+
     //hit this route so users can upVote yadas
     @RequestMapping(path = "/upVote", method = RequestMethod.POST)
     public HttpStatus upVote(int yadaUserJoinId, int userId, int yadaId){
@@ -169,11 +163,23 @@ public class YadaRestController {
 
         return HttpStatus.ACCEPTED;
     }
+    //route which brings user to the editing screen with scraped website text and submission box
+    @RequestMapping(path = "/lemmieYada", method = RequestMethod.GET)
+    public ArrayList<String> letMeYada(@RequestParam (value = "url", required = false) String url) throws IOException {
+
+        ArrayList<String> scrapedSite = soupThatSite(url);
+
+        return scrapedSite;
+    }
 
     //hit this route to display yadas for a given webpage from the chrome extension
     @RequestMapping(path = "/lemmieSeeTheYadas", method = RequestMethod.GET)
-    public Iterable<Yada> showMeTheYada(@RequestParam (value = "url", required = false) String url) {
+    public Iterable<Yada> showMeTheYada(HttpSession session, @RequestParam (value = "url", required = false) String url) {
 
+        String username = (String) session.getAttribute("username");
+        if (username == null) {
+
+        }
         Link link = links.findFirstByUrl(url);
         Iterable<Yada> theYadas = link.getYadaList();
 
@@ -182,13 +188,17 @@ public class YadaRestController {
 
     @RequestMapping(path = "/addYada", method = RequestMethod.POST)
     public HttpStatus addYada(String content, String url, String username) {
+
         Link link = links.findFirstByUrl(url);
         User user = users.findFirstByUsername(username);
+
         Yada yada = new Yada(content, 0, LocalDateTime.now(), 0, user, link);
         ArrayList<Yada> yadasInLink = (ArrayList<Yada>) link.getYadaList();
         yadasInLink.add(yada);
+
         yadas.save(yada);
         links.save(link);
+
         return HttpStatus.ACCEPTED;
     }
 
@@ -203,11 +213,14 @@ public class YadaRestController {
 
             Random r = new Random();
             int yadaTotalVotesIterator = minimum + r.nextInt((maximum - minimum) + 1);
+
             link.setNumberOfYadas(yadaCountIterator);
             link.setTotalVotes(yadaTotalVotesIterator);
             link.setLinkScore(link.getTotalVotes() / link.getNumberOfYadas());
+
             yadaCountIterator++;
             links.save(link);
+
             sortedLinkList.add(link);
         }
         return sortedLinkList;
@@ -229,6 +242,7 @@ public class YadaRestController {
                 String str = element1.text();
                 parsedDoc.add(str);
             });
+
         } else {
 
             doc.select("h1").stream().filter(Element::hasText).forEach(element1 -> {
