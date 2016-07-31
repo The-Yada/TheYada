c97cf2b18badd91ff542956046d9ede6b58569ab
willriggins <williamriggins2013@gmail.com>
2016-07-31 12:54:30 -0400

    delete regular controller, start testing

--- src/main/java/com/theironyard/entities/Link.java
+++ src/main/java/com/theironyard/entities/Link.java
@@ -55,19 +55,6 @@ public class Link {
     public Link() {
     }
 
-    public Link(String url, LocalDateTime timeOfCreation, double linkScore, int totalVotes, int upVotes, int downVotes, int numberOfYadas, long timeDiffInSeconds, String title, int karma) {
-        this.url = url;
-        this.timeOfCreation = timeOfCreation;
-        this.linkScore = linkScore;
-        this.totalVotes = totalVotes;
-        this.upVotes = upVotes;
-        this.downVotes = downVotes;
-        this.numberOfYadas = numberOfYadas;
-        this.timeDiffInSeconds = timeDiffInSeconds;
-        this.title = title;
-        this.karma = karma;
-    }
-
     public Link(String url, LocalDateTime timeOfCreation, double linkScore, int totalVotes, int upVotes, int downVotes, double controversyScore, int numberOfYadas, long timeDiffInSeconds, String title, int karma, List<Yada> yadaList) {
         this.url = url;
         this.timeOfCreation = timeOfCreation;
@@ -80,32 +67,6 @@ public class Link {
         this.timeDiffInSeconds = timeDiffInSeconds;
         this.title = title;
         this.karma = karma;
-        this.yadaList = yadaList;
-
-    }
-
-    public Link(String url, LocalDateTime timeOfCreation, double linkScore, int totalVotes, int upVotes, int downVotes, double controversyScore, int numberOfYadas, long timeDiffInSeconds, String title, int karma, List<Yada> yadaList) {
-        this.url = url;
-        this.timeOfCreation = timeOfCreation;
-        this.linkScore = linkScore;
-        this.totalVotes = totalVotes;
-        this.upVotes = upVotes;
-        this.downVotes = downVotes;
-        this.controversyScore = controversyScore;
-        this.numberOfYadas = numberOfYadas;
-        this.timeDiffInSeconds = timeDiffInSeconds;
-        this.title = title;
-        this.karma = karma;
-        this.yadaList = yadaList;
-    }
-
-    public Link(String url, LocalDateTime timeOfCreation, double linkScore, int totalVotes, int numberOfYadas, long timeDiffInSeconds, List<Yada> yadaList) {
-        this.url = url;
-        this.timeOfCreation = timeOfCreation;
-        this.linkScore = linkScore;
-        this.totalVotes = totalVotes;
-        this.numberOfYadas = numberOfYadas;
-        this.timeDiffInSeconds = timeDiffInSeconds;
         this.yadaList = yadaList;
     }
 
