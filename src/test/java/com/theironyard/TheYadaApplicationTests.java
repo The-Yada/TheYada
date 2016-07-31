62b75c4f5b622dce8159e3b7cf07a934bfbc38e1
willriggins <williamriggins2013@gmail.com>
2016-07-31 15:32:50 -0400

    login test workin

--- src/test/java/com/theironyard/TheYadaApplicationTests.java
+++ src/test/java/com/theironyard/TheYadaApplicationTests.java
@@ -10,13 +10,18 @@ import com.theironyard.services.YadaRepository;
 import com.theironyard.services.YadaUserJoinRepository;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -27,6 +32,7 @@ import java.util.ArrayList;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = TheYadaApplication.class)
 @WebAppConfiguration
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TheYadaApplicationTests {
 
 	@Autowired
@@ -52,55 +58,83 @@ public class TheYadaApplicationTests {
 	}
 
 	@Test
-	public void addYada() throws Exception {
-		User user = new User();
-		ArrayList<Yada> yadasInLink = new ArrayList<>();
- 		Link link = new Link("www.google.com", LocalDateTime.now(), 0, 1, 1, 0, 0, 0, 0,"asldkfja", 1, yadasInLink);
+	public void aTestLogin() throws Exception {
+		User user = new User("Tom", "password");
 
-		users.save(user);
-		links.save(link);
+		ObjectMapper om = new ObjectMapper();
+		String json = om.writeValueAsString(user);
 
-		Yada yada = new Yada();
-		yada.setContent("Test Yada");
-		yada.setDownvotes(0);
-		yada.setKarma(1);
-		yada.setUpvotes(1);
-		yada.setUser(user);
-		yada.setTime(LocalDateTime.now());
-		yada.setLink(link);
-		yadas.save(yada);
-
-
-		ObjectMapper mapper = new ObjectMapper();
-		String json = mapper.writeValueAsString(yada);
 
 		mockMvc.perform(
-				MockMvcRequestBuilders.post("/addYada")
+				MockMvcRequestBuilders.post("/login")
 						.content(json)
 						.contentType("application/json")
-		);
 
-		Assert.assertTrue(yadas.count() == 1);
+		);
+		Assert.assertTrue(user.getUsername().equals("Tom") && users.count() == 1);
 	}
 
 	@Test
-	public void testLemmeSeeTheYadas() throws Exception {
-		User user = new User("testName", "testPass");
-
-
-		ObjectMapper mapper = new ObjectMapper();
-		String json = mapper.writeValueAsString(user);
-
-		mockMvc.perform(
-				MockMvcRequestBuilders.get("/lemmieSeeTheYadas")
-						.content(json)
-						.contentType("application/json")
-		);
-
-		Assert.assertTrue(link.getYadaList().size() > 0);
-		Assert.assertTrue(!testUrl.contains("?"));
+	public void cTestUpvote() throws Exception {
 
 	}
+
+
+
+
+
+
+//	@Test
+//	public void addYada() throws Exception {
+//		User user = new User();
+//		ArrayList<Yada> yadasInLink = new ArrayList<>();
+// 		Link link = new Link("www.google.com", LocalDateTime.now(), 0, 1, 1, 0, 0, 0, 0,"asldkfja", 1, yadasInLink);
+//
+//		users.save(user);
+//		links.save(link);
+//
+//		Yada yada = new Yada();
+//		yada.setContent("Test Yada");
+//		yada.setDownvotes(0);
+//		yada.setKarma(1);
+//		yada.setUpvotes(1);
+//		yada.setUser(user);
+//		yada.setTime(LocalDateTime.now());
+//		yada.setLink(link);
+//		yadas.save(yada);
+//
+//
+//		ObjectMapper mapper = new ObjectMapper();
+//		String json = mapper.writeValueAsString(yada);
+//
+//		mockMvc.perform(
+//				MockMvcRequestBuilders.post("/addYada")
+//						.content(json)
+//						.contentType("application/json")
+//		);
+//
+//		Assert.assertTrue(yadas.count() == 1);
+//	}
+
+
+//	@Test
+//	public void testLemmeSeeTheYadas() throws Exception {
+//
+//		ResultActions ra = mockMvc.perform(
+//				MockMvcRequestBuilders.get("/lemmieSeeTheYadas")
+//		);
+//
+//		MvcResult result = ra.andReturn();
+//		MockHttpServletResponse resp = result.getResponse();
+//		String json = resp.getContentAsString();
+//
+//		ObjectMapper mapper = new ObjectMapper();
+//		Yada yada = mapper.readValue(json, Yada.class);
+//
+//
+//
+//		Assert.assertTrue(
+//	}
 
 }
 
