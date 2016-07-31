ca2b29c6e0c478989b8faed1e3e251a53aca9495
willriggins <williamriggins2013@gmail.com>
2016-07-31 13:33:56 -0400

    commit for pulling tests

--- src/test/java/com/theironyard/TheYadaApplicationTests.java
+++ src/test/java/com/theironyard/TheYadaApplicationTests.java
@@ -1,5 +1,14 @@
 package com.theironyard;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.theironyard.entities.Link;
+import com.theironyard.entities.User;
+import com.theironyard.entities.Yada;
+import com.theironyard.services.LinkRepository;
+import com.theironyard.services.UserRepository;
+import com.theironyard.services.YadaRepository;
+import com.theironyard.services.YadaUserJoinRepository;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,8 +17,13 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = TheYadaApplication.class)
@@ -19,11 +33,42 @@ public class TheYadaApplicationTests {
 	@Autowired
 	WebApplicationContext wap;
 
+	@Autowired
+	UserRepository users;
+
+	@Autowired
+	YadaRepository yadas;
+
+	@Autowired
+	YadaUserJoinRepository yadaUserJoinRepo;
+
+	@Autowired
+	LinkRepository links;
+
 	MockMvc mockMvc;
 
 	@Before
 	public void before() {
 		mockMvc = MockMvcBuilders.webAppContextSetup(wap).build();
+	}
+
+	@Test
+	public void testLemmeSeeTheYadas() throws Exception {
+		User user = new User("testName", "testPass");
+
+
+		ObjectMapper mapper = new ObjectMapper();
+		String json = mapper.writeValueAsString(user);
+
+		mockMvc.perform(
+				MockMvcRequestBuilders.get("/lemmieSeeTheYadas")
+				.content(json)
+				.contentType("application/json")
+		);
+
+		Assert.assertTrue(link.getYadaList().size() > 0);
+		Assert.assertTrue(!testUrl.contains("?"));
+
 	}
 
 }
